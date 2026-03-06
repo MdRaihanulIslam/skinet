fed5ce15515400b0e5f45b054db99e0763d4f850
mdraihanulislam <raihan.islam@orbitax.com>
2026-03-06 16:06:40 +0600

    End of section 2

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product>products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguaration).Assembly);
+        }
+    }
+}
